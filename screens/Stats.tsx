797dc6bacf7bc36df5a7fa602b4eeb032c03ecaa
elijah-systems <elijahndungu30@gmail.com>
2020-10-25 23:25:56 +0300

    Design Mode

--- screens/Stats.tsx
+++ screens/Stats.tsx
@@ -1,30 +1,26 @@
-import * as React from 'react';
-import { StyleSheet, Image, TouchableOpacity, Box } from 'react-native';
+import * as React from "react";
+import { StyleSheet, Image, TouchableOpacity, Box } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { Text, View } from '../components/Themed';
+import { Text, View } from "../components/Themed";
 
 const Stats = () => {
-    <View style={styles.container}> 
-      <View style={styles.statContainer}>
-       <Text style={styles.statNumber}>Stats</Text>
-      </View>
+  <View style={styles.container}>
+    <View style={styles.statContainer}>
+      <Text style={styles.statNumber}>Stats</Text>
     </View>
-
-}
+  </View>;
+};
 
 const styles = StyleSheet.create({
   container: {
-      flex:0.7,
-      marginHorizontal: 16,
-      borderRadius: 16,
-    
+    flex: 0.7,
+    marginHorizontal: 16,
+    borderRadius: 16,
   },
   statContainer: {
-      backgroundColor: '#ff33aaff',
-      marginBottom: 20
-
-  }
-
+    backgroundColor: "#ff33aaff",
+    marginBottom: 20,
+  },
 });
 
 export default Stats;
--- screens/dist/TabOneScreen.js
+++ screens/dist/TabOneScreen.js
@@ -10,37 +10,36 @@ function TabOneScreen() {
         React.createElement(Themed_1.Text, { style: styles.title }, "~Digital Ticket~"),
         React.createElement(Themed_1.View, { style: styles.separator, lightColor: "#eee", darkColor: "rgba(255,255,255,0.1)" }),
         React.createElement(Themed_1.Text, { style: styles.about }, "Easy solution to buy tickets for your travel, business trips, transportation, lodging and culnary."),
-        React.createElement(react_native_1.TouchableOpacity, { style: styles["continue"], onPress: function () {
-            } },
+        React.createElement(react_native_1.TouchableOpacity, { style: styles["continue"], onPress: function () { } },
             React.createElement(vector_icons_1.Ionicons, { name: "md-arrow-round-forward", size: 37, color: "#000" }))));
 }
 exports["default"] = TabOneScreen;
 var styles = react_native_1.StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+        alignItems: "center",
+        justifyContent: "center"
     },
     title: {
         fontSize: 30,
-        fontFamily: 'Nunito_Regular'
+        fontFamily: "Nunito_Regular"
     },
     separator: {
         marginVertical: 30,
         height: 1,
-        width: '80%'
+        width: "80%"
     },
     about: {
         fontSize: 20,
-        textAlign: 'center',
-        fontFamily: 'Nunito_Regular'
+        textAlign: "center",
+        fontFamily: "Nunito_Regular"
     },
     "continue": {
-        backgroundColor: 'transparent',
+        backgroundColor: "transparent",
         height: 40,
         marginTop: 20,
-        width: '70%',
-        alignItems: 'center',
+        width: "70%",
+        alignItems: "center",
         borderRadius: 20
     }
 });
